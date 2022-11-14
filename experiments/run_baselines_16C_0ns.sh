86f992433bba01c18c8480bb79ba56ec7a9cbbc2
PKUZHOU <751722308@qq.com>
2022-11-14 19:55:02 +0800

    add baseline 16C

--- experiments/run_baselines_16C_0ns.sh
+++ experiments/run_baselines_16C_0ns.sh
@@ -1,9 +1,9 @@
-exp_tag=baseline_16C
+exp_tag=baseline_16C_50m
 mem_channels=4
 num_cores=16
 dram_io=4800
 cxl_bw=128000
 
 cd ..
-python run_experiments.py --exp_tag $exp_tag  --results_dir ./experiments/isca/ --trace_dir ./traces/  --l2_pref="['no', 'bop_new','streamer','scooby']" --pfb_pref="['no']" --dram_io=$dram_io --mem_channels=$mem_channels --warmup_inst=10000000 --sim_inst=10000000 --num_cores=$num_cores
+python run_experiments.py --exp_tag $exp_tag  --results_dir ./experiments/isca/ --trace_dir ./traces/  --l2_pref="['no', 'bop_new','streamer','scooby']" --pfb_pref="['no']" --dram_io=$dram_io --mem_channels=$mem_channels --warmup_inst=50000000 --sim_inst=50000000 --num_cores=$num_cores
 
